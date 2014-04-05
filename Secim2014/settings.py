6c50222e296f1908339882de1608c218630164a5
root <root@ip-172-31-39-119.us-west-2.compute.internal>
2014-04-05 16:41:44 +0000

    uwsgi + nginx support

--- Secim2014/settings.py
+++ Secim2014/settings.py
@@ -55,7 +55,9 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
